aa2c2d624ae47946953e921fa643e034b8a98641
tripathik9559 <tripathik9559@gmail.com>
2026-06-06 09:52:38 +0530

    fix: static storage no manifest hashing, images working

--- Artistic_Avenue/settings.py
+++ Artistic_Avenue/settings.py
@@ -76,7 +76,8 @@ USE_TZ = False
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'aa_app', 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
